3fb51bb30ad0bee5436aa55d0f5a6639755f3576
MonikaSharmag <91123313+MonikaSharmag@users.noreply.github.com>
2021-09-28 16:46:18 +0530

    To find even number
    even/odd

---
+++ EvenOrOdd.java
@@ -0,0 +1,13 @@
+//Q 4 wap to check is number is even or odd.
+
+public class EvenOrOdd {
+    public static void main (String[] args){
+        int a=95;
+        if(a%2==0){
+            System.out.println("even number");
+        }
+        else{
+            System.out.println("odd number");
+        }
+    }
+}
